f4995dd2bea0a4b83245063cd0cd9a152b5a166a
kitce <kitce.dev@gmail.com>
2019-03-01 19:05:29 +0800

    Changed `<[type]>` to `as [type]` as it is preferred and looks better

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@ import App from './components/App';
 
 const app = new App('Basic example');
 
-const root = <HTMLElement>document.getElementById('root');
+const root = document.getElementById('root') as HTMLElement;
 root.appendChild(app.render());
