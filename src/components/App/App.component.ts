5a5690d0a63cf92221582a7792b14dd310b88de9
kitce <kitce.dev@gmail.com>
2019-11-26 18:18:40 +0800

    Follow ESLint

--- src/components/App/App.component.ts
+++ src/components/App/App.component.ts
@@ -2,15 +2,19 @@ import Vue from 'vue';
 import Component from 'vue-class-component';
 import styles from './styles.scss';
 
-const template: string = require('./App.html');
+const template: string = require('./App.html'); // eslint-disable-line @typescript-eslint/no-var-requires
+
+interface IData {
+  message: string;
+}
 
 @Component({
   template
 })
 export default class App extends Vue {
   styles = styles;
-  
-  data () {
+
+  data (): IData {
     return {
       message: 'Vue example'
     };
