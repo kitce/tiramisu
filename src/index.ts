8a0cfa0451e59d112d0d411476a3b34c129a69e3
kitce <kitce.dev@gmail.com>
2019-12-03 13:51:31 +0800

    Changed "Basic" to "Vanilla"

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import './index.scss';
 import App from './components/App';
 
-const app = new App('Basic example');
+const app = new App('Vanilla example');
 
 const root = document.getElementById('root') as HTMLElement;
 root.appendChild(app.render());
