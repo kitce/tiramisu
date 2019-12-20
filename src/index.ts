28730004d8b68c057ed48f57331c562a126ac85b
kitce <kitce.dev@gmail.com>
2019-12-20 16:20:38 +0800

    Updated main.ts

--- src/index.ts
+++
@@ -1,7 +0,0 @@
-import './index.scss';
-import App from './components/App';
-
-const app = new App('Vanilla example');
-
-const root = document.getElementById('root') as HTMLElement;
-root.appendChild(app.render());
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import './main.scss';
+import App from './components/App/App.component';
 
-console.info('Delicious');
+const app = new App('Vanilla example');
+
+const root = document.getElementById('root') as HTMLElement;
+root.appendChild(app.render());
 
 if (module.hot) {
   module.hot.accept();
